fa73f2084caa649e39e86b4470405ebd146c7390
TashaBolsh <SerdcevaTanyaN@gmail.ru>
2023-09-25 11:53:29 +0300

    add Kabzda

---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export function Rating()
+{
+    console.log("Rating rendering")
+    return (
+        <div>
+            <Star selected = {true}/>
+            <Star selected = {true}/>
+            <Star selected = {false}/>
+            <Star selected = {false}/>
+            <Star selected = {false}/>
+        </div>
+    )
+}
+
+function Star(props: any) {
+    console.log("Star rendering");
+    if (props.selected === true) {
+        return <div><b>star</b></div>
+    } else {
+        return <div>star</div>
+    }
+}
